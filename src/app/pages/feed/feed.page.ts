d5d0efe384c8317144fc2b77cad18452963d0b9b
jem1420 <jemaemon04@gmail.com>
2020-09-23 23:50:46 -0800

    Update feed page UI

--- src/app/pages/feed/feed.page.ts
+++ src/app/pages/feed/feed.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { ModalController, LoadingController } from '@ionic/angular';
 import { CartService } from 'src/app/services/cart.service';
 import { BehaviorSubject } from 'rxjs';
 
@@ -14,19 +14,40 @@ export class FeedPage implements OnInit {
   slider: any = [];
   cartItemCount: BehaviorSubject<number>;
 
+  sliderOptions={
+    autoplay:{
+      delay:2000
+    }, loop :true
+  }
 
-  constructor(private cartService: CartService, private modalCtrl: ModalController) { }
+  constructor(private cartService: CartService, private modalCtrl: ModalController, private LoadController: LoadingController) { }
 
-  ngOnInit() {
+  async ngOnInit() {
     this.slider = this.cartService.getSlider();
     this.product = this.cartService.getProduct();
     this.cartItemCount = this.cartService.getCartItemCount();
+    const loader:HTMLIonLoadingElement = await this.LoadController.create({
+      message:'Getting Products..',
+      spinner: 'circles',
+      animated: true
+    })
+      await loader.present().then();
+   
+
+    setTimeout(async () => {
+      await loader.dismiss().then();
+      
+    },4000);
+
   }
 
   addToCart(product){
     this.cartService.addProduct(product);
   }
 
+
+
+  
 
 
 }
